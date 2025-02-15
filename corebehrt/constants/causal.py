a08943d9653498ac3df1cd30d6b41c9ddeceface
Kiril Klein <kikl@di.ku.dk>
2025-02-15 21:10:20 +0100

    refactor: update constants

--- corebehrt/constants/causal.py
+++ corebehrt/constants/causal.py
@@ -6,6 +6,9 @@ OUTCOMES = "outcomes"
 CF_OUTCOMES = "cf_outcomes"
 CF_PROBAS = "cf_probas"
 
+PROBAS_CONTROL_COL = "probas_control"
+PROBAS_EXPOSED_COL = "probas_exposed"
+
 EXPOSURE_COL = "exposure"
 PS_COL = "ps"
 
@@ -19,3 +22,4 @@ SIMULATION_RESULTS_FILE = "probas_and_outcomes.csv"
 
 EXPERIMENT_DATA_FILE = "experiment_data.parquet"
 EXPERIMENT_STATS_FILE = "experiment_stats.csv"
+ESTIMATE_RESULTS_FILE = "estimate_results.csv"
