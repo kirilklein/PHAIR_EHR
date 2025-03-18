77c29cfe03016bed4d984910bd2c1299b3b877f6
Kiril Klein <kikl@di.ku.dk>
2025-03-18 10:34:13 +0100

    Update data_module to use unix epoch

--- corebehrt/modules/trainer/data_module.py
+++ corebehrt/modules/trainer/data_module.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from os.path import join
 from typing import Any, Dict, List, Tuple
 
@@ -14,7 +13,7 @@ from corebehrt.functional.causal.load import (
 )
 from corebehrt.functional.cohort_handling.outcomes import get_binary_outcomes
 from corebehrt.functional.utils.filter import filter_folds_by_pids
-from corebehrt.functional.utils.time import get_abspos_from_origin_point
+from corebehrt.functional.utils.time import get_hours_since_epoch
 from corebehrt.modules.trainer.dataset import SimpleDataset
 
 
@@ -80,10 +79,7 @@ class EncodedDataModule:
         index_dates = pd.read_csv(
             join(self.cfg.paths.cohort, INDEX_DATES_FILE), parse_dates=[TIMESTAMP_COL]
         )
-        origin_point = {"year": 2020, "month": 1, "day": 26}
-        index_dates[ABSPOS_COL] = get_abspos_from_origin_point(
-            index_dates[TIMESTAMP_COL], datetime(**origin_point)
-        )
+        index_dates[ABSPOS_COL] = get_hours_since_epoch(index_dates[TIMESTAMP_COL])
         folds = torch.load(join(self.cfg.paths.cohort, FOLDS_FILE))
         _, pids = load_encodings_and_pids_from_encoded_dir(self.cfg.paths.encoded_data)
         return index_dates, folds, pids
