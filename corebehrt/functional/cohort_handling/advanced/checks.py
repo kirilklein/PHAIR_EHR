9321b6331ee1746fa661dce347be441af0e3f573
Kiril Klein <kikl@di.ku.dk>
2025-04-12 16:56:09 +0200

    minor fixes for checks and correct path in cfg

--- corebehrt/functional/cohort_handling/advanced/checks.py
+++ corebehrt/functional/cohort_handling/advanced/checks.py
@@ -75,6 +75,10 @@ def check_delays_config(delays_config: dict) -> None:
     """Check that delays_config has valid codes (which should be strings) and days (which should be ints)"""
     if len(delays_config) == 0:
         return
+    if CODE_GROUPS not in delays_config or DAYS not in delays_config:
+        raise ValueError(
+            f"delays_config must contain both '{CODE_GROUPS}' and '{DAYS}' keys"
+        )
     for code_group in delays_config[CODE_GROUPS]:
         if not isinstance(code_group, str):
             raise ValueError(f"Code group for delays must be a string")
--- corebehrt/functional/cohort_handling/advanced/utills.py
+++ corebehrt/functional/cohort_handling/advanced/utills.py
@@ -10,12 +10,18 @@ from corebehrt.constants.cohort import (
 def print_stats(stats: Dict):
     print("----- Criteria Application Summary -----")
     print(f"Initial total: {stats['initial_total']}")
-    print("\nExcluded by individual criteria:")
+
+    print("\nExcluded by individual inclusion criteria:")
     for k, v in stats[EXCLUDED_BY_INCLUSION_CRITERIA].items():
         print(f"  - {k}: {v}")
-    if stats[EXCLUDED_BY_EXCLUSION_CRITERIA]:
-        print("\nExcluded by unique code limits:")
-        for k, v in stats[N_EXCLUDED_BY_CODE_LIMITS].items():
-            print(f"  - {k}: {v}")
+
+    print("\nExcluded by individual exclusion criteria:")
+    for k, v in stats[EXCLUDED_BY_EXCLUSION_CRITERIA].items():
+        print(f"  - {k}: {v}")
+
+    print("\nExcluded by unique code limits:")
+    for k, v in stats[N_EXCLUDED_BY_CODE_LIMITS].items():
+        print(f"  - {k}: {v}")
+
     print(f"\nFinal included: {stats[FINAL_INCLUDED]}")
     print("----------------------------------------")
