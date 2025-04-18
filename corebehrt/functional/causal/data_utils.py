d5fd6ad836c512638887fb7000f9a7164b57ca3f
kirilklein <kleinkiril94@gmail.com>
2025-04-18 12:52:19 -0400

    additional checks

--- corebehrt/functional/causal/data_utils.py
+++ corebehrt/functional/causal/data_utils.py
@@ -16,6 +16,18 @@ def align_df_with_pids(df: pd.DataFrame, pids: list) -> pd.DataFrame:
     Returns:
         The aligned DataFrame.
     """
+
+    # Check if PID_COL is in df
+    if PID_COL not in df.columns:
+        raise ValueError(f"PID_COL is not in the DataFrame: {df.columns}")
+
+    # Validate that all patient IDs exist in the DataFrame
+    missing_pids = set(pids) - set(df[PID_COL])
+    if missing_pids:
+        raise ValueError(
+            f"The following patient IDs are not in the DataFrame: {missing_pids}"
+        )
+
     df2 = df.set_index(PID_COL)
     df2 = df2.reindex(pids)
     return df2.reset_index()
