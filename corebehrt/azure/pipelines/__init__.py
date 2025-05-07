f608584075da2df973aa1677f9396c2fc2e759d7
Kiril Klein <kikl@di.ku.dk>
2025-05-07 22:04:09 +0200

    rename

--- corebehrt/azure/pipelines/__init__.py
+++ corebehrt/azure/pipelines/__init__.py
@@ -1,5 +1,5 @@
 from corebehrt.azure.pipelines.E2E import E2E
 from corebehrt.azure.pipelines.FINETUNE import FINETUNE
-from corebehrt.azure.pipelines.ESTIMATE import FINETUNE_ESTIMATE
+from corebehrt.azure.pipelines.FINETUNE_ESTIMATE import FINETUNE_ESTIMATE
 
 PIPELINE_REGISTRY = [E2E, FINETUNE, FINETUNE_ESTIMATE]
