6705a9f77f61f5b5cf948b77c939eabf198bab75
Kiril Klein <kikl@di.ku.dk>
2025-06-29 18:32:57 +0200

    docs: improve extract death

--- corebehrt/functional/preparation/causal/extract.py
+++ corebehrt/functional/preparation/causal/extract.py
@@ -3,15 +3,14 @@ import numpy as np
 from corebehrt.modules.preparation.causal.dataset import CausalPatientData
 
 
-def extract_death(patient: CausalPatientData, death_token: int) -> int:
+def extract_death(patient: CausalPatientData, death_token: int) -> float:
     """
     Extracts the death absolute position from a patient.
-    If the patient does not have a death, returns None.
     Args:
         patient: CausalPatientData
         death_token: int
     Returns:
-        int: The death absolute position or None if the patient does not have a death.
+        float: The death absolute position or np.nan if the patient does not have a death.
     """
     if death_token in patient.concepts:
         return patient.abspos[patient.concepts.index(death_token)]
