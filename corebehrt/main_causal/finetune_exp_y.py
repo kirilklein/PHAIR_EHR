1fbaca00932363df19517527a924ecaafa661220
Kiril Klein <kikl@di.ku.dk>
2025-07-13 15:53:51 +0200

    Refactor ft script

--- corebehrt/main_causal/finetune_exp_y.py
+++ corebehrt/main_causal/finetune_exp_y.py
@@ -1,11 +1,12 @@
 import logging
 import os
 from os.path import join
+from corebehrt.modules.setup.config import Config
 
 # os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 import torch
 
-from corebehrt.constants.causal.data import EXPOSURE, OUTCOME, OUTCOME_PREFIX
+from corebehrt.constants.causal.data import EXPOSURE, OUTCOME
 from corebehrt.constants.paths import FOLDS_FILE, PREPARED_ALL_PATIENTS, TEST_PIDS_FILE
 from corebehrt.functional.setup.args import get_args
 from corebehrt.main.helper.finetune_cv import check_for_overlap
@@ -38,21 +39,14 @@ def main_finetune(config_path):
     train_val_pids = data.get_pids()
 
     # If evaluation is desired, then:
-    #   - If test_pids are present in the prepared data directory, use them
     if cfg.get("evaluate", False):
         if os.path.exists(join(cfg.paths.prepared_data, TEST_PIDS_FILE)):
             test_pids = torch.load(join(cfg.paths.prepared_data, TEST_PIDS_FILE))
             test_data = data.filter_by_pids(test_pids)
-    train_val_data = data.filter_by_pids(train_val_pids)
 
     # Use folds from prepared data
-    folds_path = join(cfg.paths.prepared_data, FOLDS_FILE)
-    folds = torch.load(folds_path)
-    torch.save(folds, join(cfg.paths.model, FOLDS_FILE))
-    check_for_overlap(folds, test_pids, logger)
-    n_folds = len(folds)
-    logger.info(f"Using {n_folds} predefined folds")
-
+    folds = handle_folds(cfg, test_pids, logger)
+    train_val_data = data.filter_by_pids(train_val_pids)
     cv_loop(
         cfg,
         logger,
@@ -62,38 +56,45 @@ def main_finetune(config_path):
         test_data,
     )
 
-    # Get outcome names from config
     outcome_names = data.get_outcome_names()
-
-    # Compute scores for exposure
-    compute_and_save_scores_mean_std(
-        n_folds, cfg.paths.model, mode="val", target_type=EXPOSURE
-    )
-
-    # Compute scores for each outcome individually
-    if outcome_names:
-        compute_and_save_scores_mean_std(
-            n_folds,
-            cfg.paths.model,
-            mode="val",
-            target_type=OUTCOME,
-            outcome_names=outcome_names,
-        )
-
+    save_combined_scores(cfg, len(folds), outcome_names, "val")
     if len(test_data) > 0:
-        compute_and_save_scores_mean_std(
-            n_folds, cfg.paths.model, mode="test", target_type=EXPOSURE
-        )
-        if outcome_names:
-            compute_and_save_scores_mean_std(
-                n_folds,
-                cfg.paths.model,
-                mode="test",
-                target_type=OUTCOME,
-                outcome_names=outcome_names,
-            )
+        save_combined_scores(cfg, len(folds), outcome_names, "test")
 
     logger.info("Done")
+
+
+def handle_folds(cfg: Config, test_pids: list, logger: logging.Logger) -> list:
+    """
+    Load folds and check for overlap with test pids.
+    Save folds to model directory.
+    Return folds.
+    """
+    folds_path = join(cfg.paths.prepared_data, FOLDS_FILE)
+    folds = torch.load(folds_path)
+    check_for_overlap(folds, test_pids, logger)
+    n_folds = len(folds)
+    logger.info(f"Using {n_folds} predefined folds")
+    torch.save(folds, join(cfg.paths.model, FOLDS_FILE))
+    return folds
+
+
+def save_combined_scores(
+    cfg: Config, n_folds: int, outcome_names: list, mode: str
+) -> None:
+    """
+    Save combined scores for exposure and outcomes.
+    """
+    compute_and_save_scores_mean_std(
+        n_folds, cfg.paths.model, mode=mode, target_type=EXPOSURE
+    )
+    compute_and_save_scores_mean_std(
+        n_folds,
+        cfg.paths.model,
+        mode=mode,
+        target_type=OUTCOME,
+        outcome_names=outcome_names,
+    )
 
 
 if __name__ == "__main__":
