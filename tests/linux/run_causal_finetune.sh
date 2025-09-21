977de27ea9c9f940c6200b528e668b62912a4e51
Kiril Klein <kikl@di.ku.dk>
2025-09-21 17:43:26 +0200

    Add new bash scripts for causal pipeline execution
    - Introduced `run_causal_finetune.sh` for streamlined execution of the causal finetuning process with error checking.
    - Added `run_causal_simulated_baseline.sh` to manage simulated baseline experiments, including outcome simulation and cohort selection.
    - Updated `run_causal.sh` to enhance the pipeline with improved error handling and streamlined steps for preprocessing, training, and evaluation.
    - Each script includes inline error checking to ensure robust execution and clear feedback on failures.

--- tests/linux/run_causal_finetune.sh
+++ tests/linux/run_causal_finetune.sh
@@ -17,40 +17,8 @@ check_error() {
     fi
 }
 
-echo "======================================"
-echo "==== Running Preprocessing and Pretraining... ===="
-echo "==== Deleting old features... ===="
-rm -rf outputs/causal/data/features
-
-echo "==== Running create_data... ===="
-python -m corebehrt.main.create_data --config_path corebehrt/configs/causal/prepare_and_pretrain/create_data.yaml
-check_error
-
-echo "==== Running prepare_training_data... ===="
-python -m corebehrt.main.prepare_training_data --config_path corebehrt/configs/causal/prepare_and_pretrain/prepare_pretrain.yaml
-check_error
-
-echo "==== Running pretrain... ===="
-python -m corebehrt.main.pretrain --config_path corebehrt/configs/causal/prepare_and_pretrain/pretrain.yaml
-check_error
-
-# Run Outcomes and Cohort Selection
-echo "==== Running create_outcomes... ===="
-python -m corebehrt.main.create_outcomes --config_path corebehrt/configs/causal/outcomes.yaml
-check_error
-
-echo "==== Running select_cohort... ===="
-python -m corebehrt.main_causal.select_cohort_full --config_path corebehrt/configs/causal/select_cohort_full/extract.yaml
-check_error
-
-echo "======================================"
-echo "==== Running prepare_finetune_data... ===="
-python -m corebehrt.main_causal.prepare_ft_exp_y --config_path corebehrt/configs/causal/finetune/prepare/simple.yaml
-check_error
-
-echo "==== Testing prepare_data_ft_exp_y... ===="
-python tests/pipeline/prepare_data_ft_exp_y.py ./outputs/causal/finetune/prepared_data
-check_error
+# Run the pipeline with inline error checking
+# Run Preprocessing and Pretraining
 
 echo "==== Running finetune... ===="
 python -m corebehrt.main_causal.finetune_exp_y --config_path corebehrt/configs/causal/finetune/simple.yaml
